578b4c0dc765faa8544dc0b1252d3e54fd5e531f
lieyunye <lieyunye@gmail.com>
2015-10-15 15:18:52 +0800

    Initial Commit

---
+++ audioBarGraphWave/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  audioBarGraphWave
+//
+//  Created by lieyunye on 10/15/15.
+//  Copyright © 2015 lieyunye. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
